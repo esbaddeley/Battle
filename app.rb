f03ae54b8b8f44983354d024bde70b8aea0542f1
Eilw <eirikwiig@gmail.com>
2016-02-03 10:47:46 +0000

    added redirect function

--- app.rb
+++ app.rb
@@ -2,14 +2,22 @@ require 'sinatra/base'
 
 class Battle < Sinatra::Base
 
+  enable :sessions
+
   get '/' do
     erb(:index)
   end
 
   post '/names' do
-    @player_1 = params['Player 1']
-    @player_2 = params['Player 2']
-    erb(:play)
+    session[:player_1] = params['Player 1']
+    session[:player_2] = params['Player 2']
+    redirect '/play'
+  end
+
+  get '/play' do
+    @player_1 = session[:player_1]
+    @player_2 = session[:player_2]
+    erb :play
   end
 
 
